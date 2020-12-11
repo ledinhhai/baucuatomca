cb48500de53aa431db10d8f47b8a64f216d011dd
Le Dinh Hai <hailedinh.uit@gmail.com>
2020-12-11 13:50:50 +0700

    Updates

---
+++ precache-manifest.91486332fad7d61b3fb500f4d2bf4312.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabd33e8952c1325f619714d6a0bcc0e",
+    "url": "/baucuatomca/index.html"
+  },
+  {
+    "revision": "f3fc5e7baaf0be3aae5a",
+    "url": "/baucuatomca/static/css/2.334dfab6.chunk.css"
+  },
+  {
+    "revision": "a88e2e36d1f2362d7753",
+    "url": "/baucuatomca/static/css/main.5f94996c.chunk.css"
+  },
+  {
+    "revision": "f3fc5e7baaf0be3aae5a",
+    "url": "/baucuatomca/static/js/2.81cfe0d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/baucuatomca/static/js/2.81cfe0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88e2e36d1f2362d7753",
+    "url": "/baucuatomca/static/js/main.a5dd2475.chunk.js"
+  },
+  {
+    "revision": "e1dd404c65c10e44b66a",
+    "url": "/baucuatomca/static/js/runtime-main.834a664c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/baucuatomca/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/baucuatomca/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/baucuatomca/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/baucuatomca/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/baucuatomca/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
